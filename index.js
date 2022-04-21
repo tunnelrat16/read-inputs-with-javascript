75984dbe5827130e8ddc5d367e48b4919ac96d06
tunnelrat16 <tjandmatt@hotmail.com>
2022-04-20 20:20:04 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function logValues(){
-  // Your code here
+const $form = document.querySelector("form")
+
+function logValues() {
+    $form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const firstName = formData.get("first-name");
+        const lastName = formData.get("last-name");
+        const email = formData.get("email");
+        console.log(firstName, lastName, email);
+    });
 }
